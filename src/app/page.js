ceee0dcd40ebcf7d0856cec4028533bed9b604e3
3llack <ayomiidx@gmail.com>
2025-07-30 16:50:34 +0100

    Build fix 4

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import Nav from './components/welcome.js'
 import Hero from './components/hero.js'
 import Cta_section from './components/ctaOne.js'
-import Main_one from './components/mainOne.js'
+import Main_one from './components/mainone.js'
 import Main_two from './components/mainTwo.js'
 
 
