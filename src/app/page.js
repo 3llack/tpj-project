bd3e9fed32e51473f731259807dc1af3663c74fa
3llack <ayomiidx@gmail.com>
2025-07-30 16:37:32 +0100

    Build fix 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Nav from './components/welcome.js'
 import Hero from './components/hero.js'
-import Cta_section from './components/cta_one.js'
-import Main_one from './components/main_one.js'
-import Main_two from './components/main_two.js'
+import Cta_section from './components/ctaOne.js'
+import Main_one from './components/mainOne.js'
+import Main_two from './components/mainTwo.js'
 
 
 export default function Home() {
