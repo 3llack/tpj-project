99513ac4f9c093f98970eb8dde1c600a9d1d9dbc
Omolayo Oshadami <oshas29@gmail.com>
2025-07-31 15:48:15 +0100

    responsive changes to nav made

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,13 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Hero from "./components/hero.js";
-import Cta_section from "./components/ctaOne.js";
-import Main_one from "./components/mainone.js";
-import Main_two from "./components/mainTwo.js";
+import Hero from "../components/hero.js";
+import Cta_section from "../components/ctaOne.js";
+import Main_one from "../components/mainone.js";
+import Main_two from "../components/mainTwo.js";
 
 export default function Home() {
   return (
     <>
-      <p>Home page</p>
+      <p>Contact page</p>
 
       <section className="hero" id="hero">
         <Hero />
